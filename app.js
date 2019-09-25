c073712c5ab6189a736d4a682edacde47433c83f
RiinaPiiroinen <55059598+RiinaPiiroinen@users.noreply.github.com>
2019-09-25 11:05:21 +0300

    Add files via upload
    Päivitetty versio

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+
+/*CORS isn’t enabled on the server, this is due to security reasons by default,
+
+so no one else but the webserver itself can make requests to the server.*/
+
+app.use(function(req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+   
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   
+    next();
+   
+    });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
 
 // Luodaan yhteys
 const db = mysql.createConnection({
@@ -16,10 +41,10 @@ db.connect((err) => {
     console.log('MySql Connected...');
 });
 
-const app = express();
 
-// Luo taulu
-app.get('/teetietokanta', (req, res) => {
+
+// Luo tietokanta
+app.post('/teetietokanta', (req, res) => {
     let sql = 'CREATE DATABASE db';
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -28,6 +53,7 @@ app.get('/teetietokanta', (req, res) => {
     });
 });
 
+//Tietokannan käyttöönotto
 app.get('/tietokannankayttoonotto', (req, res) => {
     let sql = 'USE db';
     db.query(sql, (err, result) => {
@@ -38,7 +64,7 @@ app.get('/tietokannankayttoonotto', (req, res) => {
 });
 
 // Luodaan taulu
-app.get('/createregistrationtable', (req, res) => {
+app.post('/createregistrationtable', (req, res) => {
     let sql = 'CREATE TABLE registration(id int not null PRIMARY KEY, first varchar(255) , last varchar(255), age int)';
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -48,7 +74,7 @@ app.get('/createregistrationtable', (req, res) => {
 });
 
 //Asetetaan tiedot
-app.get('/asetatiedot', (req, res) => {
+app.post('/asetatiedot', (req, res) => {
     let sql = "INSERT INTO registration(id,first, last, age) VALUES(1,'Aku','Ankka',45);";
     db.query(sql, (err, result) => {
         if(err) throw err;
@@ -59,7 +85,7 @@ app.get('/asetatiedot', (req, res) => {
 
 
 // Tietueen päivitys
-app.get('/paivita/:id', (req, res) => {
+app.put('/paivita/:id', (req, res) => {
     let newTitle = 'Updated Title';
     let sql = `UPDATE registration SET title = '${newTitle}' WHERE id = ${req.params.id}`;
     let query = db.query(sql, (err, result) => {
@@ -69,8 +95,10 @@ app.get('/paivita/:id', (req, res) => {
     });
 });
 
+
+
 // Tietueen poisto
-app.get('/poista/:id', (req, res) => {
+app.delete('/poista/:id', (req, res) => {
     let newTitle = 'Updated Title';
     let sql = `DELETE FROM registration WHERE id = ${req.params.id}`;
     let query = db.query(sql, (err, result) => {
@@ -79,6 +107,8 @@ app.get('/poista/:id', (req, res) => {
         res.send('Poistettu...');
     });
 });
+
+
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
